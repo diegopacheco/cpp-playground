8ee60fa3278e5ae2675494c9074ff6cdb321b182
diegopacheco <diego.pacheco.it@gmail.com>
2021-02-08 17:53:30 -0300

    with jsoncpp

---
+++ jsoncpp/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <fstream>
+#include <jsoncpp/json/json.h>
+
+using namespace std;
+
+int main(){
+    ifstream ifs("data.json");
+    Json::Reader reader;
+    Json::Value obj;
+    reader.parse(ifs,obj);
+    cout << "Lastname " << obj["lastname"].asString() << endl;
+    cout << "Firstname " << obj["firstname"].asString() << endl;
+    return 1;
+}
