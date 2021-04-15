249128f85e0fa79f1c985dd0276f0e4b3a77ead0
diegopacheco <diego.pacheco.it@gmail.com>
2021-04-15 20:19:48 -0300

    loops

--- loops-fun/main.cpp
+++ loops-fun/main.cpp
@@ -1,11 +1,24 @@
 #include <iostream>
 using namespace std;
 
-int main(){
+void funWithWhile(){
+    cout << "Fun with While:" << endl;
     int i = 0;
     while (i < 10){
         cout << i << "\n";
         i++;
     }
+}
+
+void funWithFor(){
+    cout << "Fun with For:" << endl;
+    for (int i = 0; i < 10; i++) {
+        cout << i << "\n";
+    }
+}
+
+int main(){
+    funWithWhile();    
+    funWithFor();
     return 0;
 }
