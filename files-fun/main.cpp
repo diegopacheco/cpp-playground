058f18b4c6b1f09bcd84dc0a525c8bc80fc3a7fc
diegopacheco <diego.pacheco.it@gmail.com>
2021-04-15 17:53:18 -0300

    files

--- files-fun/main.cpp
+++ files-fun/main.cpp
@@ -4,10 +4,19 @@
 using namespace std;
 
 int main(){
-    
-    ofstream MyFile("target/log.txt");
+    string filePath = "target/log.txt";    
+    ofstream MyFile(filePath);
     MyFile << "Files can be tricky, but it is fun enough!";
     MyFile.close();
-    cout << "DONE." << endl;
+    cout << "DONE File was written." << endl;
+
+    cout << "Reading [" << filePath << "]" << endl;
+    string myText;
+    ifstream MyReadFile(filePath);
+    while (getline(MyReadFile, myText)){
+        cout << myText;
+    }
+    MyReadFile.close();
+
     return 0;
 }
