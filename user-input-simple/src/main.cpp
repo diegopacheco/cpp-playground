85528ba734c407acf26efdd0a79a4772da27e06d
diegopacheco <diego.pacheco.it@gmail.com>
2021-04-15 21:51:17 -0300

    user input - cin

---
+++ user-input-simple/src/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    string firstName;
+    cout << "Type your first name: ";
+    cin >> firstName;
+    cout << "Your name is: " << firstName;
+    return 0;
+}
