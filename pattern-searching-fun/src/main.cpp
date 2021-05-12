4cec590fe185371a7a48a3237b338b486bf2751f
diegopacheco <diego.pacheco.it@gmail.com>
2021-05-11 23:41:57 -0300

    pattern search cpp

---
+++ pattern-searching-fun/src/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+  
+void printOccurrences(string txt, string pat){
+    int found = txt.find(pat);
+    while (found != string::npos) {
+        cout << "Pattern found at index " << found << endl;
+        found = txt.find(pat, found + 1);
+    }
+}
+  
+int main(){
+    string txt = "aaaa", pat = "aa";
+    printOccurrences(txt, pat);
+    return 0;
+}
