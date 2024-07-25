4cfc288c0be1bcdd6cbd8a9a41adde8f1accbe16
diegopacheco <diego.pacheco.it@gmail.com>
2024-07-25 02:19:19 -0700

    cpp20 modules

--- cpp-20-modules-simple/build.sh
+++ cpp-20-modules-simple/build.sh
@@ -3,4 +3,15 @@
 rm -rf target/
 mkdir -p target/
 
-g++-14 -std=c++20 -fmodules-ts -c src/*.cpp -o target/main
+# Compile each .cpp file to an object file
+for file in src/*.cpp; do
+    # Extract the filename without the extension
+    filename=$(basename -- "$file")
+    filename="${filename%.*}"
+    
+    # Compile the file to an object file
+    g++-14 -std=c++20 -fmodules-ts -c "$file" -o "target/${filename}.o"
+done
+
+# Link all object files to create the executable
+g++-14 -std=c++20 target/*.o -o target/main
