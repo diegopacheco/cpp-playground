1263042f5a8edfb962fc4ecfad54a3a9ead9f354
diegopacheco <diego.pacheco.it@gmail.com>
2024-07-25 02:20:44 -0700

    cpp20 modules

--- cpp-20-modules-simple/build.sh
+++ cpp-20-modules-simple/build.sh
@@ -2,15 +2,19 @@
 
 rm -rf target/
 mkdir -p target/
+mkdir -p gcm.cache/
 
-# Compile each .cpp file to an object file
+# Compile custom modules first (assuming helloworld.cpp defines a module)
+g++-14 -std=c++20 -fmodules-ts -c src/helloworld.cpp -o target/helloworld.o
+
+# Then compile other files
 for file in src/*.cpp; do
-    # Extract the filename without the extension
-    filename=$(basename -- "$file")
-    filename="${filename%.*}"
-    
-    # Compile the file to an object file
-    g++-14 -std=c++20 -fmodules-ts -c "$file" -o "target/${filename}.o"
+    if [[ $(basename -- "$file") != "helloworld.cpp" ]]; then
+        filename=$(basename -- "$file")
+        filename="${filename%.*}"
+        
+        g++-14 -std=c++20 -fmodules-ts -c "$file" -o "target/${filename}.o"
+    fi
 done
 
 # Link all object files to create the executable
