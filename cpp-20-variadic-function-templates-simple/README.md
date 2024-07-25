e7b135d43d43406e6bb578403d9a22fc4d6484b1
diegopacheco <diego.pacheco.it@gmail.com>
2024-07-25 01:02:29 -0700

    cpp20 variadic function template

---
+++ cpp-20-variadic-function-templates-simple/README.md
@@ -0,0 +1,21 @@
+### Result
+
+```
+❯ ./run.sh
+Var1: 
+1
+Var2: 
+Var1: 
+2
+Var2: 
+Var1: 
+3.14
+Var2: 
+Var1: 
+Pass me any number of arguments
+Var2: 
+Var1: 
+I will print
+
+Var2: 
+```
--- cpp-20-variadic-function-templates-simple/src/main.cpp
+++ cpp-20-variadic-function-templates-simple/src/main.cpp
@@ -1,9 +1,14 @@
 #include <iostream>
 using namespace std;
 
+void print() {}
+
 template <typename T, typename... Types>
 void print(T var1, Types... var2){
+    cout << "Var1: " << endl;
     cout << var1 << endl;
+
+    cout << "Var2: " << endl;
     print(var2...);
 }
  
