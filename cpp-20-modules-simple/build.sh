4a88c982ce042fea62bcf7dd479df2c74ee5584b
diegopacheco <diego.pacheco.it@gmail.com>
2024-07-25 02:18:14 -0700

    cpp20 modules

--- cpp-20-modules-simple/build.sh
+++ cpp-20-modules-simple/build.sh
@@ -3,11 +3,4 @@
 rm -rf target/
 mkdir -p target/
 
-# Compile the module interface
-g++-14 -std=c++20 -fmodules-ts -c src/helloworld.cpp -o target/helloworld.o
-
-# Compile the main program (assuming it imports the mymath module)
-g++-14 -std=c++20 -fmodules-ts -c src/main.cpp -o target/main.o -fmodule-mapper=target/helloworld.o
-
-# Link the object file to create the executable
-g++-14 -std=c++20 target/main.o -o target/main
+g++-14 -std=c++20 -fmodules-ts -c src/*.cpp -o target/main
