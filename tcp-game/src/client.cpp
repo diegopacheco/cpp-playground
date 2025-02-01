3a7b303784f45854235ccca984626640a3410741
diegopacheco <diego.pacheco.it@gmail.com>
2025-01-31 22:31:05 -0800

    c++ simple tcp game

--- tcp-game/src/client.cpp
+++ tcp-game/src/client.cpp
@@ -1,21 +1,16 @@
 #include <iostream>
 #include <string>
-#include <cstring>
-#include <cstdlib>
 #include <unistd.h>
 #include <arpa/inet.h>
 
 using namespace std;
 
 const int PORT = 8080;
-const char* SERVER_IP = "127.0.0.1";
 
 int main() {
     int sock = 0;
     struct sockaddr_in serv_addr;
-    char buffer[1024] = {0};
 
-    // Create socket
     if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
         cout << "Socket creation error" << endl;
         return -1;
@@ -24,8 +19,7 @@ int main() {
     serv_addr.sin_family = AF_INET;
     serv_addr.sin_port = htons(PORT);
 
-    // Convert IPv4 and IPv6 addresses from text to binary form
-    if (inet_pton(AF_INET, SERVER_IP, &serv_addr.sin_addr) <= 0) {
+    if (inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr) <= 0) {
         cout << "Invalid address/ Address not supported" << endl;
         return -1;
     }
@@ -42,20 +36,25 @@ int main() {
     send(sock, name.c_str(), name.size(), 0);
 
     int guess;
+    char buffer[1024] = {0}; // Declare buffer only once
     while (true) {
         cout << "Guess a number (1-100): ";
         cin >> guess;
-        send(sock, &guess, sizeof(guess), 0);
-        
+        string guessStr = to_string(guess);
+        send(sock, guessStr.c_str(), guessStr.size(), 0);
+
         // Receive result from server
-        string result;
-        recv(sock, &result, sizeof(result), 0);
-        
-        if (result == "1") {
-            cout << "Congratulations! You guessed the correct number!" << endl;
-            break;
-        } else {
-            cout << result << endl;
+        int bytesRead = recv(sock, buffer, sizeof(buffer), 0);
+        if (bytesRead > 0) {
+            buffer[bytesRead] = '\0';
+            string result(buffer);
+
+            if (result.find("Congratulations") != string::npos) {
+                cout << result << endl;
+                break;
+            } else {
+                cout << result << endl;
+            }
         }
     }
 
