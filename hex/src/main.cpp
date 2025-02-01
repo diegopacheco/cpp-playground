c051703fb8d8aa6564354f86674a714fb88ffeef
diegopacheco <diego.pacheco.it@gmail.com>
2025-01-31 22:50:57 -0800

    c++ hex

--- hex/src/main.cpp
+++ hex/src/main.cpp
@@ -1,7 +1,25 @@
 #include <iostream>
+#include <sstream>
+#include <iomanip>
 using namespace std;
 
-int main(){
-    cout << "C++ works" << endl;
+string stringToHex(const string& input) {
+    stringstream ss;
+    for (char c : input) {
+        ss << hex << setw(2) << setfill('0') << (int)c;
+    }
+    return ss.str();
+}
+
+int main(int argc, char* argv[]) {
+    if (argc < 2) {
+        cout << "Usage: " << argv[0] << " <string>" << endl;
+        return 1;
+    }
+
+    string input = argv[1];
+    string hexOutput = stringToHex(input);
+
+    cout << "Hexadecimal representation: " << hexOutput << endl;
     return 0;
 }
