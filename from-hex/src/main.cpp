80b6d3264c3b54c2c90c9439b7011c59761eb6db
diegopacheco <diego.pacheco.it@gmail.com>
2025-01-31 22:58:14 -0800

    c++ hex

--- from-hex/src/main.cpp
+++ from-hex/src/main.cpp
@@ -1,7 +1,29 @@
 #include <iostream>
+#include <sstream>
+#include <string>
+#include <vector>
+
 using namespace std;
 
-int main(){
-    cout << "C++ works" << endl;
+string hex_to_string(const string& hex) {
+    string result;
+    for (size_t i = 0; i < hex.length(); i += 2) {
+        string byte = hex.substr(i, 2);
+        char chr = (char)(int)strtol(byte.c_str(), nullptr, 16);
+        result.push_back(chr);
+    }
+    return result;
+}
+
+int main(int argc, char* argv[]) {
+    if (argc < 2) {
+        cout << "Usage: " << argv[0] << " <hex_string>" << endl;
+        return 1;
+    }
+
+    string hex_string = argv[1];
+    string result = hex_to_string(hex_string);
+    cout << "Hex: " << hex_string << endl;
+    cout << "String: " << result << endl;
     return 0;
 }
