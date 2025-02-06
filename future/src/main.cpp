2c5d04c9c77908c816337ffca200eff23f4d12d1
diegopacheco <diego.pacheco.it@gmail.com>
2025-02-06 00:57:13 -0800

    c++ futures

---
+++ future/src/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <future>
+
+int compute() {
+    return 42;
+}
+
+int main() {
+    std::future<int> result = std::async(compute);
+    std::cout << "Result: " << result.get() << '\n';
+    return 0;
+}
