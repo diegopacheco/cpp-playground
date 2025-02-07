23b1d123f16017f46c1767258723368c68437a92
diegopacheco <diego.pacheco.it@gmail.com>
2025-02-07 00:10:13 -0800

    c++ lock_guard

---
+++ lock_guard/src/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <thread>
+#include <mutex>
+
+std::mutex mtx;
+
+void printMessage(const std::string& msg) {
+    std::lock_guard<std::mutex> lock(mtx);
+    std::cout << msg << '\n';
+}
+
+int main() {
+    std::thread t1(printMessage, "Hello from thread 1");
+    std::thread t2(printMessage, "Hello from thread 2");
+    t1.join();
+    t2.join();
+    return 0;
+}
